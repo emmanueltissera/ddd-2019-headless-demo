e6cf213afa87dfca7aa366c5b2501eb05de2eeec
Emmanuel Tissera <emmanuel@luminary.com>
2019-08-08 16:04:49 +1000

    Added initial files for demo

---
+++ script.js
@@ -0,0 +1,91 @@
+var lastModifiedDateRetrieved;
+
+function checkLastModifiedDate() {
+    hideAlert();
+    $.ajax({
+        type: "GET",
+        url: "https://deliver.kenticocloud.com/1c7a9e17-7ff3-0235-eb49-1d37c47ae828/items/?system.type=display_ad&limit=1&order=system.last_modified[desc]&elements=title",
+        dataType: "json",
+        success: actOnDateCheck,
+        error: function() {
+            showAlert("JSON call to CaaS failed - modification check");
+        }
+    });
+}
+
+function showAlert(message) {
+    $(".alert-bottom").html(message);
+    $(".alert-bottom").show();
+}
+
+function hideAlert() {
+    $(".alert-bottom").hide();
+}
+
+function actOnDateCheck(data) {
+    var lastModifiedDate = data.items[0].system.last_modified;
+    console.log("Last modification date from KC: " + lastModifiedDate);
+    if (lastModifiedDateRetrieved !== lastModifiedDate) {
+        getNewSlides();
+        lastModifiedDateRetrieved = lastModifiedDate;
+    }
+}
+
+function getNewSlides() {
+    $.ajax({
+        type: "GET",
+        url: "https://deliver.kenticocloud.com/1c7a9e17-7ff3-0235-eb49-1d37c47ae828/items/?system.type=display_ad&limit=3&order=system.last_modified[desc]",
+        dataType: "json",
+        success: processData,
+        error: function() {
+            showAlert("JSON call to CaaS failed - retrieve slides");
+        }
+    });
+}
+
+// Process the retrieved data. 
+function processData(data) {
+    console.log("Retrieved data from KC");
+    console.log(data);
+
+    var slideIndex = 0;
+
+    resetCarousel();
+
+    data.items.map(function(item) {
+        $(createSlide(item)).appendTo('.carousel-inner');
+        $(createIndicator(slideIndex)).appendTo('.carousel-indicators');
+        slideIndex++;
+    });
+
+    $('.carousel-item').first().addClass('active');
+    $('.carousel-indicators > li').first().addClass('active');
+    $('#carouselSlideContainer').carousel('cycle');
+}
+
+function resetCarousel() {
+    $('.carousel-item').remove();
+    $('.carousel-indicators > li').remove();
+    $('#carouselSlideContainer').carousel('dispose');
+}
+
+function createSlide(item) {
+    var carouselItem = '<div class="carousel-item" style="background-image: url(\'' + item.elements.slide_image.value[0].url + '\')">' +
+        '<div class="carousel-caption d-md-block">' +
+        '<h2 class="display-4">' + item.elements.title.value + '</h2>' +
+        item.elements.description.value +
+        '</div>' +
+        '</div>';
+    return carouselItem;
+}
+
+function createIndicator(index) {
+    var indicatorItem = '<li data-target="#carouselSlideContainer" data-slide-to="' + index + '"></li>'
+    return indicatorItem;
+}
+
+checkLastModifiedDate();
+
+var intervalMins = 2;
+var intervalMilliSeconds = 1000 * 60 * intervalMins;
+setInterval(checkLastModifiedDate, intervalMilliSeconds);
